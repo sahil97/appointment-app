12539ac2e50f4711495b01baa06b030249ed2ff8
sahil97 <sahil.sharma071997@gmail.com>
2020-01-27 11:07:28 +0530

    temp changes

--- ms-frontend/src/App.js
+++ ms-frontend/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {
+    BrowserRouter,
+    Route,
+    Switch
+} from "react-router-dom";
+import Dashboard from './containers/Dashboard/Dashboard';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-
-     </div>
+            <BrowserRouter>
+                <Switch>
+                  <Route path="/" component={Dashboard} />
+                </Switch>
+            </BrowserRouter>
+        </div>
     );
 }
 
